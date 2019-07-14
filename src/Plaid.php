fdb97d58abde54933d8c86b04e0aed3d7b6b331c
Brent Scheffler <brent@brentscheffler.com>
2019-07-14 08:08:44 -0700

    Adding support for Asset Reports and Audit copies.

--- src/Plaid.php
+++ src/Plaid.php
@@ -196,7 +196,7 @@ final class Plaid
         }
 
         return \json_decode($response->getBody()->getContents());
-    }
+	}
 
     /**
      * Build a PSR-7 Request instance.
@@ -533,5 +533,157 @@ final class Plaid
         return $this->doRequest(
             $this->buildRequest("post", "income/get", $this->clientCredentials($params))
         );
-    }
+	}
+
+	/**
+	 * Create an Asset Report.
+	 *
+	 * @param array<string> $access_tokens
+	 * @param integer $days_requested
+	 * @param array<string, string> $options
+	 * @return object
+	 */
+	public function createAssetReport(array $access_tokens, int $days_requested, array $options = []): object
+	{
+		$params = [
+			'access_tokens' => $access_tokens,
+			'days_requested' => $days_requested,
+			'options' => (object) $options
+		];
+
+		return $this->doRequest(
+			$this->buildRequest("post", "asset_report/create", $this->clientCredentials($params))
+		);
+	}
+
+	/**
+	 * Refresh an Asset Report.
+	 *
+	 * @param string $asset_report_token
+	 * @param integer $days_requested
+	 * @param array<string, string> $options
+	 * @return object
+	 */
+	public function refreshAssetReport(string $asset_report_token, int $days_requested, array $options = []): object
+	{
+		$params = [
+			'asset_report_token' => $asset_report_token,
+			'days_requested' => $days_requested,
+			'options' => (object) $options
+		];
+
+		return $this->doRequest(
+			$this->buildRequest("post", "asset_report/refresh", $this->clientCredentials($params))
+		);
+	}
+
+	/**
+	 * Filter an Asset Report by specifying which Accounts to exclude.
+	 *
+	 * @param string $asset_report_token
+	 * @param array<string> $exclude_accounts
+	 * @return object
+	 */
+	public function filterAssetReport(string $asset_report_token, array $exclude_accounts): object
+	{
+		$params = [
+			'asset_report_token' => $asset_report_token,
+			'account_ids_to_exclude' => $exclude_accounts
+		];
+
+		return $this->doRequest(
+			$this->buildRequest("post", "asset_report/filter", $this->clientCredentials($params))
+		);
+	}
+
+	/**
+	 * Get an Asset report.
+	 *
+	 * @param string $asset_report_token
+	 * @param boolean $include_insights
+	 * @return object
+	 */
+	public function getAssetReport(string $asset_report_token, bool $include_insights = false): object
+	{
+		$params = [
+			"asset_report_token" => $asset_report_token,
+			"include_insights" => $include_insights
+		];
+
+		return $this->doRequest(
+			$this->buildRequest("post", "asset_report/get", $this->clientCredentials($params))
+		);
+	}
+
+	/**
+	 * Get an Asset report in PDF format.
+	 *
+	 * @param string $report_token
+	 * @param boolean $include_insights
+	 * @return object
+	 */
+	public function getAssetReportPdf(string $asset_report_token, bool $include_insights = false): object
+	{
+		$params = [
+			"asset_report_token" => $asset_report_token,
+			"include_insights" => $include_insights
+		];
+
+		return $this->doRequest(
+			$this->buildRequest("post", "asset_report/pdf/get", $this->clientCredentials($params))
+		);
+	}
+
+	/**
+	 * Remove an Asset Report.
+	 *
+	 * @param string $asset_report_token
+	 * @return object
+	 */
+	public function removeAssetReport(string $asset_report_token): object
+	{
+		$params = [
+			"asset_report_token" => $asset_report_token
+		];
+
+		return $this->doRequest(
+			$this->buildRequest("post", "asset_report/remove", $this->clientCredentials($params))
+		);
+	}
+
+	/**
+	 * Create an Audit Copy of an Asset Report.
+	 *
+	 * @param string $asset_report_token
+	 * @param string $auditor_id
+	 * @return object
+	 */
+	public function createAssetReportAuditCopy(string $asset_report_token, string $auditor_id): object
+	{
+		$params = [
+			"asset_report_token" => $asset_report_token,
+			"auditor_id" => $auditor_id
+		];
+
+		return $this->doRequest(
+			$this->buildRequest("post", "asset_report/audit_copy/create", $this->clientCredentials($params))
+		);
+	}
+
+	/**
+	 * Remove an Audit Copy.
+	 *
+	 * @param string $audit_copy_token
+	 * @return object
+	 */
+	public function removeAssetReportAuditCopy(string $audit_copy_token): object
+	{
+		$params = [
+			"audit_copy_token" => $audit_copy_token
+		];
+
+		return $this->doRequest(
+			$this->buildRequest("post", "asset_report/audit_copy/remove", $this->clientCredentials($params))
+		);
+	}
 }
