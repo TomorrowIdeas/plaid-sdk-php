ffebeaad1e74f81baecdb79ebd6feb3b91bfb18f
strawbee <yuejiao.hou@gmail.com>
2019-07-19 11:32:53 -0700

    add liabilities test

---
+++ tests/LiabilitiesTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace TomorrowIdeas\Plaid\Tests;
+
+/**
+ * @covers TomorrowIdeas\Plaid\Plaid
+ */
+class LiabilitiesTest extends TestCase
+{
+    public function test_get_liabilities()
+    {
+        $response = $this->getPlaidClient()->getLiabilities("access_token");
+
+        $this->assertEquals("POST", $response->method);
+        $this->assertEquals("Plaid-Version: 2019-05-29", $response->version);
+        $this->assertEquals("Content-Type: application/json", $response->content);
+        $this->assertEquals("/liabilities/get", $response->path);
+        $this->assertEquals("client_id", $response->params->client_id);
+        $this->assertEquals("secret", $response->params->secret);
+        $this->assertEquals("access_token", $response->params->access_token);
+        $this->assertEquals(new \StdClass, $response->params->options);
+    }
+}
