eb1861a693ccf054aefb961e8ee8407411b6088e
Brent Scheffler <brent@brentscheffler.com>
2020-10-17 09:49:00 -0700

    Adding test to check for InvalidArgumentException when an unsupported interval is given.

--- tests/PaymentScheduleEntityTest.php
+++ tests/PaymentScheduleEntityTest.php
@@ -3,6 +3,7 @@
 namespace TomorrowIdeas\Plaid\Tests;
 
 use DateTime;
+use InvalidArgumentException;
 use TomorrowIdeas\Plaid\Entities\PaymentSchedule;
 
 /**
@@ -29,5 +30,11 @@ class PaymentScheduleEntityTest extends TestCase
 			"2020-10-01",
 			$payment_schedule->getStartDate()->format("Y-m-d")
 		);
+	}
+
+	public function test_invalid_interval_throws_invalid_argument_exception(): void
+	{
+		$this->expectException(InvalidArgumentException::class);
+		new PaymentSchedule("YEARLY", 1, new DateTime("2020-01-01"));
 	}
 }
